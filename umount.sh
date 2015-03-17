35b9f2d4d546d69793f539780d2ded1a6127de05
Boris HUISGEN <bhuisgen@hbis.fr>
2015-03-18 00:25:26 +0100

    Use config file
    Signed-off-by: Boris HUISGEN <bhuisgen@hbis.fr>

--- umount.sh
+++ umount.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-MOUNT_PATH=$(dirname $0)/store
+set -e
+
+if [ -r $(dirname $0)/config ]; then
+    echo "config file not found/not readable"
+    
+    exit 1
+fi
+
+. $(dirname $0)/config
 
 umount $MOUNT_PATH
-
-echo "Backup store unmounted"
