7986ad17d32eb338d4139d193f18a446ba03940e
Boris HUISGEN <bhuisgen@hbis.fr>
2015-03-18 00:25:42 +0100

    Use config file and custom BUP_PATH
    Signed-off-by: Boris HUISGEN <bhuisgen@hbis.fr>

--- mount.sh
+++ mount.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-MOUNT_PATH=$(dirname $0)/store
-MOUNT_OPTIONS=""
+set -e
+
+if [ -r $(dirname $0)/config ]; then
+    echo "config file not found/not readable"
+    
+    exit 1
+fi
+
+. $(dirname $0)/config
+
+if [ !-z "$1" ]; then
+    BUP_PATH=$1
+fi
 
 mkdir -p $MOUNT_PATH
-bup fuse $MOUNT_OPTIONS $MOUNT_PATH
 
-echo "Backup store mounted in $MOUNT_PATH"
+bup fuse $MOUNT_OPTIONS $MOUNT_PATH
